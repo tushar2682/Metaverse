65361ca5ad5cb0bf3744c00f25adf6f8e7e101e5
tushar2682 <tuniyal158@gmail.com>
2026-01-22 08:34:03 +0530

    Remove old Tests/index.test.js

--- Tests/index.test.js
+++ Tests/index.test.js
@@ -515,86 +515,86 @@ describe("Arena endpoints", () => {
         });
         spaceId = spaceResponse.data.spaceId;
     });
-});
-test("Incorrect spaceId returns a 400", async () => {
-    const response = await axios.get(`${BACKEND_URL}/api/v1/space/123kasdk01`, {
-        headers: {
-            "authorization": `Bearer ${userToken}`
-        }
-    });
-    expect(response.status).toBe(400)
-})
 
-test("Correct spaceId returns all the elements", async () => {
-    const response = await axios.get(`${BACKEND_URL}/api/v1/space/${spaceId}`, {
-        headers: {
-            "authorization": `Bearer ${userToken}`
-        }
-    });
-    console.log(response.data)
-    expect(response.data.dimensions).toBe("100x200")
-    expect(response.data.elements.length).toBe(3)
-})
-test("Delete endpoint is able to delete an element", async () => {
-    const response = await axios.get(`${BACKEND_URL}/api/v1/space/${spaceId}`, {
-        headers: {
-            "authorization": `Bearer ${userToken}`
-        }
-    });
+    test("Incorrect spaceId returns a 400", async () => {
+        const response = await axios.get(`${BACKEND_URL}/api/v1/space/123kasdk01`, {
+            headers: {
+                "authorization": `Bearer ${userToken}`
+            }
+        });
+        expect(response.status).toBe(400)
+    })
 
-    console.log(response.data.elements[0].id)
-    let res = await axios.delete(`${BACKEND_URL}/api/v1/space/element`, {
-        data: { id: response.data.elements[0].id },
-        headers: {
-            "authorization": `Bearer ${userToken}`
-        }
-    });
+    test("Correct spaceId returns all the elements", async () => {
+        const response = await axios.get(`${BACKEND_URL}/api/v1/space/${spaceId}`, {
+            headers: {
+                "authorization": `Bearer ${userToken}`
+            }
+        });
+        console.log(response.data)
+        expect(response.data.dimensions).toBe("100x200")
+        expect(response.data.elements.length).toBe(3)
+    })
+    test("Delete endpoint is able to delete an element", async () => {
+        const response = await axios.get(`${BACKEND_URL}/api/v1/space/${spaceId}`, {
+            headers: {
+                "authorization": `Bearer ${userToken}`
+            }
+        });
+
+        console.log(response.data.elements[0].id)
+        let res = await axios.delete(`${BACKEND_URL}/api/v1/space/element`, {
+            data: { id: response.data.elements[0].id },
+            headers: {
+                "authorization": `Bearer ${userToken}`
+            }
+        });
 
 
-    const newResponse = await axios.get(`${BACKEND_URL}/api/v1/space/${spaceId}`, {
-        headers: {
-            "authorization": `Bearer ${userToken}`
-        }
-    });
+        const newResponse = await axios.get(`${BACKEND_URL}/api/v1/space/${spaceId}`, {
+            headers: {
+                "authorization": `Bearer ${userToken}`
+            }
+        });
 
-    expect(newResponse.data.elements.length).toBe(2)
-})
+        expect(newResponse.data.elements.length).toBe(2)
+    })
 
-test("Adding an element fails if the element lies outside the dimensions", async () => {
-    const newResponse = await axios.post(`${BACKEND_URL}/api/v1/space/element`, {
-        "elementId": element1Id,
-        "spaceId": spaceId,
-        "x": 10000,
-        "y": 210000
-    }, {
-        headers: {
-            "authorization": `Bearer ${userToken}`
-        }
-    });
+    test("Adding an element fails if the element lies outside the dimensions", async () => {
+        const newResponse = await axios.post(`${BACKEND_URL}/api/v1/space/element`, {
+            "elementId": element1Id,
+            "spaceId": spaceId,
+            "x": 10000,
+            "y": 210000
+        }, {
+            headers: {
+                "authorization": `Bearer ${userToken}`
+            }
+        });
 
-    expect(newResponse.status).toBe(400)
-})
+        expect(newResponse.status).toBe(400)
+    })
 
-test("Adding an element works as expected", async () => {
-    await axios.post(`${BACKEND_URL}/api/v1/space/element`, {
-        "elementId": element1Id,
-        "spaceId": spaceId,
-        "x": 50,
-        "y": 20
-    }, {
-        headers: {
-            "authorization": `Bearer ${userToken}`
-        }
-    });
+    test("Adding an element works as expected", async () => {
+        await axios.post(`${BACKEND_URL}/api/v1/space/element`, {
+            "elementId": element1Id,
+            "spaceId": spaceId,
+            "x": 50,
+            "y": 20
+        }, {
+            headers: {
+                "authorization": `Bearer ${userToken}`
+            }
+        });
 
-    const newResponse = await axios.get(`${BACKEND_URL}/api/v1/space/${spaceId}`, {
-        headers: {
-            "authorization": `Bearer ${userToken}`
-        }
-    });
+        const newResponse = await axios.get(`${BACKEND_URL}/api/v1/space/${spaceId}`, {
+            headers: {
+                "authorization": `Bearer ${userToken}`
+            }
+        });
 
-    expect(newResponse.data.elements.length).toBe(3)
-})
+        expect(newResponse.data.elements.length).toBe(3)
+    })
 
 })
 
